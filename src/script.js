2482053ccb3585b9800be5f6f325fb86925953da
debracas <dommage.non@gmail.com>
2021-10-30 17:05:02 +0200

    Add basic scene with random images in VR

--- src/script.js
+++ src/script.js
@@ -19,57 +19,56 @@ function init() {
     const container = document.createElement( 'div' );
     document.body.appendChild( container );
 
+    // Scenes and controls
     scene = new THREE.Scene();
     scene.background = new THREE.Color( 0x222222 );
-
     camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.01, 50 );
     camera.position.set( 0, 1.6, 3 );
-
     controls = new OrbitControls( camera, container );
     controls.target.set( 0, 1.6, 0 );
     controls.update();
 
-    const tableGeometry = new THREE.BoxGeometry( 0.5, 0.8, 0.5 );
-    const tableMaterial = new THREE.MeshStandardMaterial( {
-        color: 0x444444,
-        roughness: 1.0,
-        metalness: 0.0
-    } );
-    const table = new THREE.Mesh( tableGeometry, tableMaterial );
-    table.position.y = 0.35;
-    table.position.z = 0.85;
-    scene.add( table );
-
-    const floorGometry = new THREE.PlaneGeometry( 4, 4 );
-    const floorMaterial = new THREE.MeshStandardMaterial( {
-        color: 0x222222,
-        roughness: 1.0,
-        metalness: 0.0
-    } );
-    const floor = new THREE.Mesh( floorGometry, floorMaterial );
-    floor.rotation.x = - Math.PI / 2;
-    scene.add( floor );
-
+    // Grid
     const grid = new THREE.GridHelper( 10, 20, 0x111111, 0x111111 );
     // grid.material.depthTest = false; // avoid z-fighting
-    scene.add( grid );
+    scene.add( grid );  
 
+    var randomImages = [];
+
+    // Add image
+    function addRandomImage() {
+        var url = "https://source.unsplash.com/random/200x200?sig=" + Math.floor(Math.random() * 100);
+        var map = new THREE.TextureLoader().load(url);
+        var material = new THREE.SpriteMaterial( { map: map, color: 0xffffff } );
+        var sprite = new THREE.Sprite( material );
+
+        let vector = new THREE.Vector3(
+            Math.random() * 2 - 1,
+            Math.random() * 2 - 1,
+            Math.random() * 2 - 1
+        );
+        vector = vector.normalize().multiplyScalar(10);
+        vector.add(camera.position);
+        sprite.position.set(vector.x, vector.y, vector.z);
+        scene.add(sprite);
+
+        randomImages.push({
+            animation: 0,
+            object: sprite
+        });
+    }
+    for(let i = 0; i < 10; i++) {
+        addRandomImage();
+    }
+    
+
+    // Lights
     scene.add( new THREE.HemisphereLight( 0x888877, 0x777788 ) );
-
     const light = new THREE.DirectionalLight( 0xffffff, 0.5 );
     light.position.set( 0, 4, 0 );
     scene.add( light );
 
-    //
-
-    const painter1 = new TubePainter();
-    scene.add( painter1.mesh );
-
-    const painter2 = new TubePainter();
-    scene.add( painter2.mesh );
-
-    //
-
+    // Renderer
     renderer = new THREE.WebGLRenderer( { antialias: true } );
     renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( window.innerWidth, window.innerHeight );
@@ -78,126 +77,20 @@ function init() {
     container.appendChild( renderer.domElement );
 
     document.body.appendChild( VRButton.createButton( renderer ) );
-
-    // controllers
-
-    function onSelectStart() {
-
-        this.userData.isSelecting = true;
-
-    }
-
-    function onSelectEnd() {
-
-        this.userData.isSelecting = false;
-
-    }
-
-    function onSqueezeStart() {
-
-        this.userData.isSqueezing = true;
-        this.userData.positionAtSqueezeStart = this.position.y;
-        this.userData.scaleAtSqueezeStart = this.scale.x;
-
-    }
-
-    function onSqueezeEnd() {
-
-        this.userData.isSqueezing = false;
-
-    }
-
-    controller1 = renderer.xr.getController( 0 );
-    controller1.addEventListener( 'selectstart', onSelectStart );
-    controller1.addEventListener( 'selectend', onSelectEnd );
-    controller1.addEventListener( 'squeezestart', onSqueezeStart );
-    controller1.addEventListener( 'squeezeend', onSqueezeEnd );
-    controller1.userData.painter = painter1;
-    scene.add( controller1 );
-
-    controller2 = renderer.xr.getController( 1 );
-    controller2.addEventListener( 'selectstart', onSelectStart );
-    controller2.addEventListener( 'selectend', onSelectEnd );
-    controller2.addEventListener( 'squeezestart', onSqueezeStart );
-    controller2.addEventListener( 'squeezeend', onSqueezeEnd );
-    controller2.userData.painter = painter2;
-    scene.add( controller2 );
-
-    //
-
-    const geometry = new THREE.CylinderGeometry( 0.01, 0.02, 0.08, 5 );
-    geometry.rotateX( - Math.PI / 2 );
-    const material = new THREE.MeshStandardMaterial( { flatShading: true } );
-    const mesh = new THREE.Mesh( geometry, material );
-
-    const pivot = new THREE.Mesh( new THREE.IcosahedronGeometry( 0.01, 3 ) );
-    pivot.name = 'pivot';
-    pivot.position.z = - 0.05;
-    mesh.add( pivot );
-
-    controller1.add( mesh.clone() );
-    controller2.add( mesh.clone() );
-
-    //
-
     window.addEventListener( 'resize', onWindowResize );
-
 }
 
 function onWindowResize() {
-
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-
     renderer.setSize( window.innerWidth, window.innerHeight );
-
-}
-
-//
-
-function handleController( controller ) {
-
-    const userData = controller.userData;
-    const painter = userData.painter;
-
-    const pivot = controller.getObjectByName( 'pivot' );
-
-    if ( userData.isSqueezing === true ) {
-
-        const delta = ( controller.position.y - userData.positionAtSqueezeStart ) * 5;
-        const scale = Math.max( 0.1, userData.scaleAtSqueezeStart + delta );
-
-        pivot.scale.setScalar( scale );
-        painter.setSize( scale );
-
-    }
-
-    cursor.setFromMatrixPosition( pivot.matrixWorld );
-
-    if ( userData.isSelecting === true ) {
-
-        painter.lineTo( cursor );
-        painter.update();
-
-    } else {
-
-        painter.moveTo( cursor );
-
-    }
-
 }
 
 function animate() {
-
     renderer.setAnimationLoop( render );
 
 }
 
 function render() {
-
-    handleController( controller1 );
-    handleController( controller2 );
-
     renderer.render( scene, camera );
-
 }
