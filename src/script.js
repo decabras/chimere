1778e2cef4f90792148ce4e0187482ce686d9cbe
debracas <dommage.non@gmail.com>
2021-10-30 16:15:12 +0200

    First vr app with threejs bundler

--- src/script.js
+++ src/script.js
@@ -1,105 +1,203 @@
 import './style.css'
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'dat.gui'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { TubePainter } from 'three/examples/jsm/misc/TubePainter.js';
+import { VRButton } from 'three/examples/jsm/webxr/VRButton.js';
 
-// Debug
-const gui = new dat.GUI()
+let camera, scene, renderer;
+let controller1, controller2;
 
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
+const cursor = new THREE.Vector3();
 
-// Scene
-const scene = new THREE.Scene()
+let controls;
 
-// Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+init();
+animate();
 
-// Materials
+function init() {
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+    const container = document.createElement( 'div' );
+    document.body.appendChild( container );
 
-// Mesh
-const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0x222222 );
 
-// Lights
+    camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.01, 50 );
+    camera.position.set( 0, 1.6, 3 );
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
+    controls = new OrbitControls( camera, container );
+    controls.target.set( 0, 1.6, 0 );
+    controls.update();
 
-/**
- * Sizes
- */
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+    const tableGeometry = new THREE.BoxGeometry( 0.5, 0.8, 0.5 );
+    const tableMaterial = new THREE.MeshStandardMaterial( {
+        color: 0x444444,
+        roughness: 1.0,
+        metalness: 0.0
+    } );
+    const table = new THREE.Mesh( tableGeometry, tableMaterial );
+    table.position.y = 0.35;
+    table.position.z = 0.85;
+    scene.add( table );
+
+    const floorGometry = new THREE.PlaneGeometry( 4, 4 );
+    const floorMaterial = new THREE.MeshStandardMaterial( {
+        color: 0x222222,
+        roughness: 1.0,
+        metalness: 0.0
+    } );
+    const floor = new THREE.Mesh( floorGometry, floorMaterial );
+    floor.rotation.x = - Math.PI / 2;
+    scene.add( floor );
+
+    const grid = new THREE.GridHelper( 10, 20, 0x111111, 0x111111 );
+    // grid.material.depthTest = false; // avoid z-fighting
+    scene.add( grid );
+
+    scene.add( new THREE.HemisphereLight( 0x888877, 0x777788 ) );
+
+    const light = new THREE.DirectionalLight( 0xffffff, 0.5 );
+    light.position.set( 0, 4, 0 );
+    scene.add( light );
+
+    //
+
+    const painter1 = new TubePainter();
+    scene.add( painter1.mesh );
+
+    const painter2 = new TubePainter();
+    scene.add( painter2.mesh );
+
+    //
+
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.outputEncoding = THREE.sRGBEncoding;
+    renderer.xr.enabled = true;
+    container.appendChild( renderer.domElement );
+
+    document.body.appendChild( VRButton.createButton( renderer ) );
+
+    // controllers
+
+    function onSelectStart() {
+
+        this.userData.isSelecting = true;
+
+    }
+
+    function onSelectEnd() {
+
+        this.userData.isSelecting = false;
+
+    }
+
+    function onSqueezeStart() {
+
+        this.userData.isSqueezing = true;
+        this.userData.positionAtSqueezeStart = this.position.y;
+        this.userData.scaleAtSqueezeStart = this.scale.x;
+
+    }
+
+    function onSqueezeEnd() {
+
+        this.userData.isSqueezing = false;
+
+    }
+
+    controller1 = renderer.xr.getController( 0 );
+    controller1.addEventListener( 'selectstart', onSelectStart );
+    controller1.addEventListener( 'selectend', onSelectEnd );
+    controller1.addEventListener( 'squeezestart', onSqueezeStart );
+    controller1.addEventListener( 'squeezeend', onSqueezeEnd );
+    controller1.userData.painter = painter1;
+    scene.add( controller1 );
+
+    controller2 = renderer.xr.getController( 1 );
+    controller2.addEventListener( 'selectstart', onSelectStart );
+    controller2.addEventListener( 'selectend', onSelectEnd );
+    controller2.addEventListener( 'squeezestart', onSqueezeStart );
+    controller2.addEventListener( 'squeezeend', onSqueezeEnd );
+    controller2.userData.painter = painter2;
+    scene.add( controller2 );
+
+    //
+
+    const geometry = new THREE.CylinderGeometry( 0.01, 0.02, 0.08, 5 );
+    geometry.rotateX( - Math.PI / 2 );
+    const material = new THREE.MeshStandardMaterial( { flatShading: true } );
+    const mesh = new THREE.Mesh( geometry, material );
+
+    const pivot = new THREE.Mesh( new THREE.IcosahedronGeometry( 0.01, 3 ) );
+    pivot.name = 'pivot';
+    pivot.position.z = - 0.05;
+    mesh.add( pivot );
+
+    controller1.add( mesh.clone() );
+    controller2.add( mesh.clone() );
+
+    //
+
+    window.addEventListener( 'resize', onWindowResize );
+
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+function onWindowResize() {
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+    renderer.setSize( window.innerWidth, window.innerHeight );
 
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 0
-camera.position.y = 0
-camera.position.z = 2
-scene.add(camera)
-
-// Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
-
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
-/**
- * Animate
- */
-
-const clock = new THREE.Clock()
-
-const tick = () =>
-{
-
-    const elapsedTime = clock.getElapsedTime()
-
-    // Update objects
-    sphere.rotation.y = .5 * elapsedTime
-
-    // Update Orbital Controls
-    // controls.update()
-
-    // Render
-    renderer.render(scene, camera)
-
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
 }
 
-tick()
+//
+
+function handleController( controller ) {
+
+    const userData = controller.userData;
+    const painter = userData.painter;
+
+    const pivot = controller.getObjectByName( 'pivot' );
+
+    if ( userData.isSqueezing === true ) {
+
+        const delta = ( controller.position.y - userData.positionAtSqueezeStart ) * 5;
+        const scale = Math.max( 0.1, userData.scaleAtSqueezeStart + delta );
+
+        pivot.scale.setScalar( scale );
+        painter.setSize( scale );
+
+    }
+
+    cursor.setFromMatrixPosition( pivot.matrixWorld );
+
+    if ( userData.isSelecting === true ) {
+
+        painter.lineTo( cursor );
+        painter.update();
+
+    } else {
+
+        painter.moveTo( cursor );
+
+    }
+
+}
+
+function animate() {
+
+    renderer.setAnimationLoop( render );
+
+}
+
+function render() {
+
+    handleController( controller1 );
+    handleController( controller2 );
+
+    renderer.render( scene, camera );
+
+}
